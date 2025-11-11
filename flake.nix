1810cb519fbe105645a52c729d95cd9bd52f48af
wangwildly-ju <wangwetly@pm.me>
2025-11-10 17:59:37 -0700

    Full multi-host/multi-user refactor (desktop/j, laptop/ju) -fix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,51 @@
 {
-   nixosConfigurations = {
+  description = "J's NixOS Configuration";
 
-    # --- Desktop ---
-    desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit self; };
-      modules = [
-        ./hosts/desktop.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.j = import ./home-j.nix;
-        }
-      ];
-    };
-
-    # --- Laptop ---
-    laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit self; };
-      modules = [
-        ./hosts/laptop.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ju = import ./home-ju.nix;
-        }
-      ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-    # ... Formatter should be here ...
+
+  
+  outputs = { self, nixpkgs, home-manager, ... }: {
+
+    # === NixOS Hosts ===
+    nixosConfigurations = {
+
+      # --- Desktop ---
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit self; };
+        modules = [
+          ./hosts/desktop.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.j = import ./home-j.nix;
+          }
+        ];
+      };
+
+      # --- Laptop ---
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit self; };
+        modules = [
+          ./hosts/laptop.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ju = import ./home-ju.nix;
+          }
+        ];
+      };
+    }; # Closes nixosConfigurations
+
+    # === Formatter ===
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-}
+
+  }; # Closes outputs
+} # Closes the entire file
