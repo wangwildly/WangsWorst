6f4fe9d66b4a9e9d8ba5541e2d1aeb1c80a166a5
wangwildly-ju <wangwetly@pm.me>
2025-11-12 12:53:17 -0700

    Discow Infernal

--- modules/network.nix
+++ modules/network.nix
@@ -7,10 +7,12 @@
     # hostName = "nixos"; # <-- This is now handled by each machines respective host file
     networkmanager.enable = true;
     # Enable firewall by default (security)
-    firewall.enable = true;
+    firewall = {
+      enable = true;
 
     # Deskflow
      allowedTCPPorts = [ 24800 ];
 
+    };
   };
 }
