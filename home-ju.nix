21c6632e6d1077bae1b081c4c0364af9590cd845
wangwildly-ju <wangwetly@pm.me>
2025-11-12 10:01:24 -0700

    Add swayidle, fix wayfire lid and brightness

--- home-ju.nix
+++ home-ju.nix
@@ -26,6 +26,7 @@
     pkgs.fastfetch
     pkgs.libsForQt5.kdeconnect-kde
     pkgs.libsecret
+    pkgs.light
 
     # Development - Keeping these here and commented out for my education
     # These are currently handled in user-level programs & config, so it manages dotfiles.
